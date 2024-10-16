f0de0b43f3db40fac92996ff7d4f746d45801d61
yayachien1234 <31569886+yayachien1234@users.noreply.github.com>
2024-10-16 09:48:52 +0800

    定義每一種方塊的形狀

--- TETRIS/Form1.cs
+++ TETRIS/Form1.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace TETRIS
 {
     public partial class Form1 : Form
@@ -5,12 +7,15 @@ namespace TETRIS
         public Form1()
         {
             InitializeComponent();
-        }
+        }  
 
-        private void Form1_Load(object sender, EventArgs e)
+        public void Form1_Load(object sender, EventArgs e)
         {
             Label[,] grids = MapGenerate.GenerateMap(this); // 調用 MapGenerate 類別中的 GenerateMap 方法
+            BlockDesign blockDesign = new BlockDesign(grids);
+            blockDesign.DrawBlock(5, 5, 11);
 
         }
+    }
     }
 }
